5ce529b76ca00b61a8192d4ab1701e8e7947230f
mehedee <mehedee@green-red.com>
2021-08-31 23:30:00 +0600

    Test for Fibonacci method added

--- tests/FibonacciTest.php
+++ tests/FibonacciTest.php
@@ -5,6 +5,29 @@ use App\Helpers\Fibonacci;
 class FibonacciTest extends TestCase
 {
 
+    public function testValidNumber()
+    {
+        $fibonacciHelper = new Fibonacci();
+        $response        = $fibonacciHelper->getValue(1);
 
+        $this->assertEquals(1, $response, 'Responded Properly');
+    }
+
+
+    public function testNegative()
+    {
+        $fibonacciHelper = new Fibonacci();
+        $response        = $fibonacciHelper->getValue(-1);
+
+        $this->assertEquals(null, $response);
+    }
+
+    public function testException()
+    {
+        $this->expectException(TypeError::class);
+
+        $fibonacciHelper = new Fibonacci();
+        $response        = $fibonacciHelper->getValue('FFF');
+    }
 
 }
