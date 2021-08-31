7c3df7e57ccc37e5ed2ab413c07ad192bbff520b
mehedee <mehedee@green-red.com>
2021-08-31 23:11:55 +0600

    Minor refactors to handle exception and negative int input

--- app/Helpers/Fibonacci.php
+++ app/Helpers/Fibonacci.php
@@ -7,9 +7,13 @@ class Fibonacci implements FibonacciInterface
 
     public function getValue(int $position): int|null
     {
+        if ($position < 0) {
+            return null;
+        }
+
         $array = [0, 1];
-        for ( $i=2; $i<=$position; ++$i ) {
-            $array[$i] = $array[$i-1] + $array[$i-2];
+        for ($i = 2; $i <= $position; ++$i) {
+            $array[$i] = $array[$i - 1] + $array[$i - 2];
         }
 
         return $array[$position];
--- app/Http/Controllers/FibonacciController.php
+++ app/Http/Controllers/FibonacciController.php
@@ -10,16 +10,21 @@ use Laravel\Lumen\Routing\Controller;
 class FibonacciController extends Controller
 {
 
+    /**
+     * @param Fibonacci $fibonacci
+     * @param $position
+     * @return Response
+     */
     public function show(Fibonacci $fibonacci, $position): Response
     {
         try {
             $value = $fibonacci->getValue($position);
-        } catch (\Exception $exception) {
+        } catch (\TypeError $exception) {
             $value = null;
         }
 
         $statusCode = $value ? 200 : 400;
-
+        
         return new Response(['value' => $value], $statusCode);
     }
 }
